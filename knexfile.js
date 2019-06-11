3c666526eb3760a3a5fa91223d13b79d83452426
Noah Steven Waldron <noah@Noahs-MacBook-Pro.local>
2019-06-11 12:55:26 -0500

    Updated knex file

--- knexfile.js
+++ knexfile.js
@@ -1,12 +1,44 @@
 module.exports = {
   development: {
-    client: 'sqlite3',
-    connection: { filename: './data/lambda.sqlite3' },
+    client: "sqlite3",
+    connection: { filename: "./data/lambda.sqlite3" },
     useNullAsDefault: true,
     migrations: {
-      directory: './data/migrations',
-      tableName: 'dbmigrations',
+      directory: "./data/migrations",
+      tableName: "dbmigrations"
     },
-    seeds: { directory: './data/seeds' },
+    seeds: { directory: "./data/seeds" }
+  }
+};
+const localPg = {
+  host: "localhost",
+  database: process.env.DB_NAME,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS
+};
+const dbConnection = process.env.DATABASE_URL;
+
+module.exports = {
+  development: {
+    client: "sqlite3",
+    connection: { filename: "./data/lambda.sqlite3" },
+    useNullAsDefault: true,
+    migrations: {
+      directory: "./data/migrations",
+      tableName: "dbmigrations"
+    },
+    seeds: { directory: "./data/seeds" }
   },
+  production: {
+    client: "pg",
+    connection: dbConnection,
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: "knex_migrations",
+      directory: "./data/migrations"
+    }
+  }
 };
